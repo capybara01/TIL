1020b1ef0047ff47303e23f93cee16ba4144564e
CAPYBARA <68233400+capybara01@users.noreply.github.com>
2020-07-26 00:11:10 +0900

    Update day2.c

--- day2.c
+++ day2.c
@@ -4,27 +4,30 @@
 
 int main(void)
 {
+        while(1)
+        {
         printf("==============================\n");
         string day = get_string("요일을 입력하세요.\n");
     
         if (strcmp("월요일", day)==0 ||strcmp("월", day)==0)
         {
-           printf("월요일: 청국장\n")
+           printf("월요일: 청국장\n");
         }
         else if (strcmp("화요일", day)==0 ||strcmp("화", day)==0)
         {
-           printf("화요일: 비빔밥\n")
+           printf("화요일: 비빔밥\n");
         }
         else if (strcmp("수요일", day)==0 ||strcmp("수", day)==0)
         {
-           printf("수요일: 된장찌개\n")
+           printf("수요일: 된장찌개\n");
         }
         else if (strcmp("목요일", day)==0 ||strcmp("목", day)==0)
         {
-           printf("목요일: 칼국수\n")
-        else if (strcmp("목요일", day)==0 ||strcmp("목", day)==0)
+           printf("목요일: 칼국수\n");
+        }
+        else if (strcmp("금요일", day)==0 ||strcmp("금", day)==0)
         {
-           printf("금요일: 칼국수\n")
+           printf("금요일: 칼국수\n");
         }
         else if (strcmp("토요일", day)==0 ||strcmp("토", day)==0)
         {
@@ -34,14 +37,14 @@ int main(void)
         {
             printf("일요일: 오삼불고기\n");
         }
-            {                
-                    break;
-            }
-            
-              else (strcmp("끝", day)==0
+        else if (strcmp("끝", day)==0)
+        {                
+            break;
+        }
+                    else
                     {
                     printf("요일을 입력하세요. ex)월요일\n");
                     }
               printf("==============================\n");
+        }
 }
-                    
